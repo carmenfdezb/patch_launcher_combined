683442c013cef9e8f4cee14d21c958ddffff9e9b
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2022-08-09 19:41:21 +0200

    Add files via upload

---
+++ ts/translation_es.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="48"/>
+        <source>Launcher settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="52"/>
+        <source>Grid portrait</source>
+        <translation>Cuadrícula vertical</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="57"/>
+        <location filename="main.qml" line="85"/>
+        <source>Columns count</source>
+        <translation>Número de columnas</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="69"/>
+        <location filename="main.qml" line="97"/>
+        <source>Rows count</source>
+        <translation>Número de filas</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="80"/>
+        <source>Grid landscape</source>
+        <translation>Cuadrícula horizontal</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="108"/>
+        <source>Common</source>
+        <translation>Común</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="113"/>
+        <source>Change font size</source>
+        <translation>Cambiar tamaño de la fuente</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="120"/>
+        <source>Change icons size</source>
+        <translation>Cambiar tamaño de iconos</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="127"/>
+        <source>Zoom value</source>
+        <translation>Valor de zoom</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="139"/>
+        <source>Show labels in edit mode</source>
+        <translation>Mostrar etiquetas en modo edición</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="146"/>
+        <source>Scroll without pages</source>
+        <translation>Desplazamiento sin páginas</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="153"/>
+        <source>Show scrollbar</source>
+        <translation>Mostrar barra de desplazamiento</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="160"/>
+        <source>Background</source>
+        <translation>Fondo</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="165"/>
+        <source>Use themed background</source>
+        <translation>Usar fondo del tema</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="173"/>
+        <source>Use system color background</source>
+        <translation>Usar color del sistema como fondo</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="180"/>
+        <source>Background opacity</source>
+        <translation>Opacidad del fondo</translation>
+    </message>
+</context>
+</TS>
