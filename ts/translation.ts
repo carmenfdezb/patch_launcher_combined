601d43aa35a6730c52501d02690631e2ca46302c
Carmen Fdez. B. <carmenfdezb@mail.com>
2026-03-04 00:21:45 +0100

    upload translation.ts

---
+++ ts/translation.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../patch/main.qml" line="48"/>
+        <source>Launcher settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="52"/>
+        <source>Grid portrait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="57"/>
+        <location filename="../patch/main.qml" line="85"/>
+        <source>Columns count</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="69"/>
+        <location filename="../patch/main.qml" line="97"/>
+        <source>Rows count</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="80"/>
+        <source>Grid landscape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="108"/>
+        <source>Common</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="113"/>
+        <source>Change font size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="120"/>
+        <source>Change icons size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="127"/>
+        <source>Zoom value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="139"/>
+        <source>Show labels in edit mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="146"/>
+        <source>Scroll without pages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="153"/>
+        <source>Show scrollbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="160"/>
+        <source>Background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="165"/>
+        <source>Use themed background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="173"/>
+        <source>Use system color background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../patch/main.qml" line="180"/>
+        <source>Background opacity</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
