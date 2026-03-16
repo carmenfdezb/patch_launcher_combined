75d5324fcc7d36edc53b35318d30e1ce9e523faa
Carmen F. B. <carmenfdezb@mail.com>
2026-03-16 20:14:34 +0100

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: patch_launcher_combined/translation
    Translate-URL: https://hosted.weblate.org/projects/patch_launcher_combined/translation/nb_NO/

--- ts/translation_nb_NO.ts
+++ ts/translation_nb_NO.ts
@@ -6,84 +6,84 @@
     <message>
         <location filename="../patch/main.qml" line="48"/>
         <source>Launcher settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Innstillinger for startskjerm</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="52"/>
         <source>Grid portrait</source>
-        <translation type="unfinished"></translation>
+        <translation>Rutenett, stående</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="57"/>
         <location filename="../patch/main.qml" line="85"/>
         <source>Columns count</source>
-        <translation type="unfinished"></translation>
+        <translation>Antall kolonner</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="69"/>
         <location filename="../patch/main.qml" line="97"/>
         <source>Rows count</source>
-        <translation type="unfinished"></translation>
+        <translation>Antall rader</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="80"/>
         <source>Grid landscape</source>
-        <translation type="unfinished"></translation>
+        <translation>Rutenett, liggende</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="108"/>
         <source>Common</source>
-        <translation type="unfinished"></translation>
+        <translation>Generelt</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="113"/>
         <source>Change font size</source>
-        <translation type="unfinished"></translation>
+        <translation>Endre skriftstørrelse</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="120"/>
         <source>Change icons size</source>
-        <translation type="unfinished"></translation>
+        <translation>Endre ikonstørrelse</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="127"/>
         <source>Zoom value</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoomverdi</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="139"/>
         <source>Show labels in edit mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis etiketter i redigeringsmodus</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="146"/>
         <source>Scroll without pages</source>
-        <translation type="unfinished"></translation>
+        <translation>Rulling uten sideskift</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="153"/>
         <source>Show scrollbar</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis rullefelt</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="160"/>
         <source>Background</source>
-        <translation type="unfinished"></translation>
+        <translation>Bakgrunn</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="165"/>
         <source>Use themed background</source>
-        <translation type="unfinished"></translation>
+        <translation>Bruk temabasert bakgrunn</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="173"/>
         <source>Use system color background</source>
-        <translation type="unfinished"></translation>
+        <translation>Bruk systemfarge for bakgrunn</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="180"/>
         <source>Background opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Gjennomsiktighet for bakgrunn</translation>
     </message>
 </context>
 </TS>
