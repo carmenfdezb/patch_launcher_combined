67466ab504d1408ef4fd3eacf665f421ed90321c
Jerome M <micucci@online.fr>
2026-08-17 03:59:24 +0200

    Translated using Weblate (French)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: patch_launcher_combined/translation
    Translate-URL: https://hosted.weblate.org/projects/patch_launcher_combined/translation/fr/

--- ts/translation_fr.ts
+++ ts/translation_fr.ts
@@ -6,84 +6,84 @@
     <message>
         <location filename="../patch/main.qml" line="48"/>
         <source>Launcher settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Paramètres du lanceur</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="52"/>
         <source>Grid portrait</source>
-        <translation type="unfinished"></translation>
+        <translation>Grille portrait</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="57"/>
         <location filename="../patch/main.qml" line="85"/>
         <source>Columns count</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre de colonnes</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="69"/>
         <location filename="../patch/main.qml" line="97"/>
         <source>Rows count</source>
-        <translation type="unfinished"></translation>
+        <translation>Nombre de lignes</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="80"/>
         <source>Grid landscape</source>
-        <translation type="unfinished"></translation>
+        <translation>Grille paysage</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="108"/>
         <source>Common</source>
-        <translation type="unfinished"></translation>
+        <translation>Paramètres communs</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="113"/>
         <source>Change font size</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifier la taille de police</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="120"/>
         <source>Change icons size</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifier la taille des icônes</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="127"/>
         <source>Zoom value</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoom</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="139"/>
         <source>Show labels in edit mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher les étiquettes en mode édition</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="146"/>
         <source>Scroll without pages</source>
-        <translation type="unfinished"></translation>
+        <translation>Défilement continu</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="153"/>
         <source>Show scrollbar</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher la barre de défilement</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="160"/>
         <source>Background</source>
-        <translation type="unfinished"></translation>
+        <translation>Arrière plan</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="165"/>
         <source>Use themed background</source>
-        <translation type="unfinished"></translation>
+        <translation>Utiliser l&apos;arrière plan du thème</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="173"/>
         <source>Use system color background</source>
-        <translation type="unfinished"></translation>
+        <translation>Utiliser le jeu de couleurs d&apos;arrière plan par défaut</translation>
     </message>
     <message>
         <location filename="../patch/main.qml" line="180"/>
         <source>Background opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Transparence de l&apos;arrière plan</translation>
     </message>
 </context>
 </TS>
